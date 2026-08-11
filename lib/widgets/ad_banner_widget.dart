dd433298d3e8f0b99f4063b33c732bc164f4c28c
Etân <noxicrocus@gmail.com>
2026-08-11 21:57:47 +0200

    ajout de vrai publicité

--- lib/widgets/ad_banner_widget.dart
+++ lib/widgets/ad_banner_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
@@ -19,15 +20,55 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
     super.initState();
     // Ne charger la pub que sur les plateformes supportées (Android/iOS natif)
     if (!kIsWeb && (defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS)) {
-      _loadAd();
+      _initAndLoad();
     }
   }
 
-  void _loadAd() {
-    final String adUnitId = defaultTargetPlatform == TargetPlatform.android
-        ? 'ca-app-pub-3940256099942544/6300978111'
-        : 'ca-app-pub-3940256099942544/2934735716';
+  Future<void> _initAndLoad() async {
+    try {
+      // 1. Récupérer la configuration depuis Firestore
+      final adsSnapshot = await FirebaseFirestore.instance
+          .collection('config')
+          .doc('ads')
+          .get();
 
+      String adUnitId;
+      final Map<String, dynamic>? data = adsSnapshot.data();
+
+      // SÉCURITÉ : Si on est en mode Debug (développement), on force les pubs de TEST
+      if (kDebugMode) {
+        adUnitId = defaultTargetPlatform == TargetPlatform.android
+            ? 'ca-app-pub-3940256099942544/6300978111'
+            : 'ca-app-pub-3940256099942544/2934735716';
+        debugPrint('AdMob: Mode développement détecté. Affichage de publicités de TEST par sécurité.');
+      } 
+      else if (adsSnapshot.exists && data != null && data['showRealAds'] == true) {
+        // UTILISER LES PUBS RÉELLES (Uniquement en mode Release)
+        if (defaultTargetPlatform == TargetPlatform.android) {
+          adUnitId = data['androidBannerId'] ?? 'ca-app-pub-3940256099942544/6300978111';
+        } else {
+          adUnitId = 'ca-app-pub-3940256099942544/2934735716';
+        }
+        debugPrint('AdMob: Mode Production détecté. Affichage des publicités RÉELLES.');
+      } 
+      else {
+        // Fallback par défaut (TEST)
+        adUnitId = defaultTargetPlatform == TargetPlatform.android
+            ? 'ca-app-pub-3940256099942544/6300978111'
+            : 'ca-app-pub-3940256099942544/2934735716';
+        debugPrint('AdMob: Configuration Firestore absente ou showRealAds=false. Mode TEST.');
+      }
+
+      // 2. Charger la bannière avec l'ID choisi
+      _loadAd(adUnitId);
+    } catch (e) {
+      debugPrint('AdMob: Erreur lors de la récupération de la config Firestore: $e');
+      // En cas d'erreur Firestore, on tente quand même de charger une pub de test
+      _loadAd('ca-app-pub-3940256099942544/6300978111');
+    }
+  }
+
+  void _loadAd(String adUnitId) {
     _bannerAd = BannerAd(
       adUnitId: adUnitId,
       request: const AdRequest(),
@@ -64,7 +105,7 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    // Cacher le widget sur les plateformes non supportées (Web, Windows, etc.)
+    // Cacher le widget sur les plateformes non supportées
     if (kIsWeb || (defaultTargetPlatform != TargetPlatform.android && defaultTargetPlatform != TargetPlatform.iOS)) {
       return const SizedBox.shrink();
     }
